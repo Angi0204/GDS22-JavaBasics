a4e3d3fa095108c51f17bbdf53dc87c55e81bd1e
Angi0204 <11839286@edu.campus02.at>
2022-09-23 17:10:37 +0200

    first change

---
+++ MyFirstClass.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+    // wir haben unsere erste Klasse erzeugt
+
+    //pvsm = Türe ins Programmieren Main Methode
+    public static void main(String[] args) {
+    //erste Ausgabe
+        System.out.println("Ich werde auf dieser Konsole angezeigt");
+
+        //args
+        //kürzel sout+tab
+        System.out.println(Arrays.toString(args));
+    }
+}
