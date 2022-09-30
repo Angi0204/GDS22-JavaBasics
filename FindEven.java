dacd106826921000160f169efd0c2c181d9489ff
Angi0204 <11839286@edu.campus02.at>
2022-09-30 19:24:29 +0200

    For-Schleife

--- FindEven.java
+++ FindEven.java
@@ -4,11 +4,11 @@ public class FindEven {
          int count=1;
          int input=10;
 
-         while(count<=input) {
+         while(count++<=input) {
              if (count%2==0)
             System.out.println("Gerade Zahlen: "+ count);
-            count++;
             //Alternativen zu count++
+             // count++;
              //count=count +1;
             // count=1+count;
              // count+=1;
